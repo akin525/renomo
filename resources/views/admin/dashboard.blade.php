ca8c04610a022e65ce3453ca7777e0f0ad5919dc
IT <akinlabisamon15@gmail.com>
2022-07-10 17:00:28 +0100

    09/07/2022

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -35,6 +35,7 @@
             </div>
         </div>
         <br>
+        @include('livewire.livewire-charts')
         <div class="row">
             <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
