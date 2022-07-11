08ed8c8ca938f47a6585c704e8a317076f3b6b0b
IT <akinlabisamon15@gmail.com>
2022-07-11 09:53:41 +0100

    09/07/2022

--- resources/views/witve.blade.php
+++ resources/views/witve.blade.php
@@ -78,7 +78,7 @@
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label for="amount">Amount</label>
-                                                <input type="text" name="amount" id="amount" class="form-control"
+                                                <input type="text" name="amount" id="amount" min="100" class="form-control"
                                                        placeholder="Enter amount" required>
                                             </div>
 
