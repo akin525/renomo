6c26e3f5b494c62a6319964c3ccdab1790faa6d4
IT <akinlabisamon15@gmail.com>
2022-07-31 14:27:37 +0100

    09/07/2022

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -165,30 +165,7 @@
                         <div class="logo_section">
                             <a href="{{ route('dashboard') }}"><img class="img-responsive" src="{{asset("images/bn.jpeg")}}" alt="#" /></a>
                         </div>
-                        {{--                        <div class="right_topbar">--}}
-                        {{--                            <div class="icon_info">--}}
-                        {{--                                <!--                                <ul>-->--}}
-                        {{--                                <!--                                    <li><a href="#"><i class="fa fa-bell-o"></i><span class="badge">2</span></a>-->--}}
-                        {{--                                <!--                                  -->--}}
-                        {{--                                <!--                                    </li>-->--}}
-                        {{--                                <!---->--}}
-                        {{--                                <!--                                    <li><a href="#"><i class="fa fa-question-circle"></i></a></li>-->--}}
-                        {{--                                <!--                                    <li><a href="#"><i class="fa fa-envelope-o"></i><span class="badge">3</span></a></li>-->--}}
-                        {{--                                <!--                                </ul>-->--}}
-                        {{--                                <ul class="user_profile_dd">--}}
-                        {{--                                    <li>--}}
-                        {{--                                        <a class="dropdown-toggle" data-toggle="dropdown"><img class="img-responsive rounded-circle" src="{{asset("images/layout_img/user_img.jpg")}}" alt="#" /><span class="name_user" > {{ Auth::user()->name }}</span></a>--}}
-                        {{--                                        <div class="dropdown-menu">--}}
-                        {{--                                            <a class="dropdown-item" href="{{ route('profile.show') }}">My Profile</a>--}}
-                        {{--                                            <a class="dropdown-item" href="profile.php">Settings</a>--}}
-                        {{--                                            <!--                                            <a class="dropdown-item" href="help.html">Help</a>-->--}}
-                        {{--                                            <a class="dropdown-item" href=""><span>Log Out</span> <i class="fa fa-sign-out"></i></a>--}}
-                        {{--                                        </div>--}}
-                        {{--                                    </li>--}}
-                        {{--                                </ul>--}}
-                        {{--                            </div>--}}
-                        {{--                        </div>--}}
-                    <  /div>
+
                 </nav>
             </div>
             <!-- end topbar -->
