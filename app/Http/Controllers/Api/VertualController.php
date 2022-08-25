d52f5ff7fa7f5fd884bb01f06de69cdde98f1be3
IT <akinlabisamson15@gmail.com>
2022-08-25 13:27:50 +0100

    web push

--- app/Http/Controllers/Api/VertualController.php
+++ app/Http/Controllers/Api/VertualController.php
@@ -143,6 +143,7 @@ class VertualController  extends Notification
                 $image='https://renomobilemoney.com/images/bn.jpeg';
 
               $this->firebasenotification($username, "Account Funded", $body);
+              $this->firebasenotificationadmin($username, "Account Funded", $body);
 
 
 
@@ -168,6 +169,38 @@ class VertualController  extends Notification
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS =>'{
     "to": "/topics/'.$username.'",
+    "notification": {
+        "body": "'.$body.'",
+        "title": "'.$title.'"
+    }
+}',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer AAAA0VPmumc:APA91bFO0BZ1BL5bGsBIFW2JGE3SZzC60y-Hrqg2UgVlgeYfj7_kIawa7W1Vz0LMTVhhyC1uy4dsSGAU2oe87HzR27PInPhLlDlWKOS5buvaejdQl2O2lWe9Ewts09GiRcmJEi3LnkzB',
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+//        dd($response);
+//        echo $response;
+    }
+    public  function firebasenotificationadmin($username, $title, $body)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://fcm.googleapis.com/fcm/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+    "to": "/topics/Adeolu23",
     "notification": {
         "body": "'.$body.'",
         "title": "'.$title.'"
