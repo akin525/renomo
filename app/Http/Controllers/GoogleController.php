7cb9ec0855ec640e8b3b1d2a260a7d7305b86a44
HP <akinlabisamson15@gmail.com>
2022-10-24 12:42:42 +0100

    giveaway

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Console\encription;
+use App\Mail\Emailotp;
 use App\Models\User;
 use App\Models\wallet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
@@ -38,6 +40,16 @@ class GoogleController extends Controller
                     'username'=>$saveUser['username'],
                     'balance' => 0,
                 ]);
+                $input=[
+                    'name'=>$user->getName(),
+                    'username'=>$saveUser['username'],
+                    'email'=>$user->getEmail(),
+                    'password'=>'Always sign in with Google',
+                ];
+                $receiver=$input ['email'];
+                $admin= 'info@renomobilemoney.com';
+                Mail::to($receiver)->send(new Emailotp($input));
+                Mail::to($admin)->send(new Emailotp($input));
             }else{
                 $saveUser = User::where('email',  encription::encryptdata($user->getEmail()))->update([
                     'google_id' => $user->getId(),
