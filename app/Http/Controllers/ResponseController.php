4905e673a9ae73b484b92dad3e11f44c1c0b41b9
HP <akinlabisamson15@gmail.com>
2022-11-09 11:26:11 +0100

    giveaway

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -9,8 +9,6 @@ class ResponseController extends Controller
 function responsefunding()
 {
     $vertual=web::latest()->get();
-$gp=json_decode(file_get_contents($vertual->webbook), true);
-return $gp;
         return view('admin/response', compact('vertual'));
 }
 }
