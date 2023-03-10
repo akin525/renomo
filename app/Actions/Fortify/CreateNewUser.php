b79e453a9a63f02561ae322c344cc998fbd9021b
HP <akinlabisamson15@gmail.com>
2023-03-10 17:48:50 +0100

    pin

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -36,11 +36,51 @@ class CreateNewUser implements CreatesNewUsers
         ])->validate();
 
         return DB::transaction(function () use ($input) {
-            $wallet= wallet::create([
-                'username' => encription::encryptdata($input['username']),
-                'balance' => 0,
-            ]);
 
+            $username=$input['username'].rand(111, 999);
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://integration.mcd.5starcompany.com.ng/api/reseller/virtual-account',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('account_name' =>  $input['name'], 'business_short_name' => 'RENO', 'uniqueid' => $username, 'email' => $input['email'], 'phone' => $input['phone'], 'webhook_url' => 'https://renomobilemoney.com/api/run',),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: mcd_key_75rq4][oyfu545eyuriup1q2yue4poxe3jfd'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            $data = json_decode($response, true);
+            if ($data['success']==1){
+                $account = $data["data"]["account_name"];
+                $number = $data["data"]["account_number"];
+                $bank = $data["data"]["bank_name"];
+                $wallet= wallet::create([
+                    'username' => encription::encryptdata($input['username']),
+                    'balance' => 0,
+                    'account_number'=>$number,
+                    'account_name'=>$account,
+                ]);
+
+
+            }elseif ($data['success']==0){
+                $wallet= wallet::create([
+                    'username' => encription::encryptdata($input['username']),
+                    'balance' => 0,
+                ]);
+
+            }
 
             $receiver=$input ['email'];
             $admin= 'info@renomobilemoney.com';
