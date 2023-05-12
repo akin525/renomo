0f9719920f961330daafaa27e70f64a21e67d058
akinl <akinlabisamson15@gmail.com>
2023-05-12 11:09:19 +0100

    new

--- app/Http/Controllers/VertualController.php
+++ app/Http/Controllers/VertualController.php
@@ -32,6 +32,77 @@ class VertualController
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://integration.mcd.5starcompany.com.ng/api/reseller/virtual-account3',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('account_name' => $name,
+                    'business_short_name' => 'RENO','uniqueid' => $username,
+                    'email' => $email,'dob' => '06/14/1986',
+                    'address' => 'Ikeja Lagos State','gender' => 'male',
+                    'phone' =>$phone,'webhook_url' => 'https://renomobilemoney.com/api/run1'),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: mcd_key_75rq4][oyfu545eyuriup1q2yue4poxe3jfd'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+//            return $response;
+//return $response;
+//var_dump(array('account_name' => $name,'business_short_name' => 'RENO','uniqueid' => $username,'email' => $email,'phone' => '08146328645', 'webhook_url'=>'https://renomobilemoney.com/go/run.php'));
+            $data = json_decode($response, true);
+            if ($data['success']==1) {
+                $account = $data["data"]["customer_name"];
+                $number = $data["data"]["account_number"];
+                $bank = $data["data"]["bank_name"];
+
+                $wallet->account_number1 = $number;
+                $wallet->account_name1 = $account;
+                $wallet->bank=$bank;
+                $wallet->save();
+
+
+//            $data = [
+//                'title' => 'Test push title',
+//                'website_id' => 1,
+//                'body' => 'Test push message',
+//                'ttl' => 300, //push notification lifetime
+//                'stretch_time' => 0, //how long to spread message delivery
+//                'link' => 'https://github.com/garethtdavies'
+//            ];
+//            SendPulse::createPushTask($data);
+                return redirect("dashboard")->withSuccess('You are not allowed to access');
+            }elseif ($data['success']==0){
+
+                Alert::error('Error', $response);
+                return redirect('myaccount');
+            }
+
+
+        }
+    }
+    public function vertual1(Request $request)
+    {
+        if (Auth::check()) {
+            $user = User::find($request->user()->id);
+            $wallet = wallet::where('username', $user->username)->first();
+
+            $username=encription::decryptdata($user->username).rand(111, 999);
+            $email=encription::decryptdata($user->email);
+            $name=encription::decryptdata($user->name);
+            $phone=encription::decryptdata($user->phone);
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
                 CURLOPT_URL => 'https://integration.mcd.5starcompany.com.ng/api/reseller/virtual-account',
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
