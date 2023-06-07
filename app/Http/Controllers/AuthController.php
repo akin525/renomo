650563844649254c5eb93f9a3b879356e5c389d2
akinl <akinlabisamson15@gmail.com>
2023-06-07 16:41:44 +0100

    new dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -265,9 +265,7 @@ Alert::success('Success', 'New Password has been sent to your email');
 //        }
         if (isset($serve)) {
             $user = User::find($request->user()->id);
-            $title = 'Delete User!';
-            $text = "Are you sure you want to delete?";
-            confirmDelete($title, $text);
+
 
             return view('select1', compact('user', 'serve'));
         }else {
