55bf623cca9e0899218e5018a709cad54522d19b
ACCOUNTS <akinlabisamson15@gmail.com>
2023-07-21 16:32:33 +0100

    cron

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -354,15 +354,15 @@
             var ctx = document.getElementById('transactionChart').getContext('2d');
 
             var chart = new Chart(ctx, {
-                type: 'area',
+                type: 'bar',
                 data: {
                     labels: data.dates,
                     datasets: [{
                         label: 'Transaction Amount',
-                        backgroundColor: 'rgb(53,169,21)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1,
                         data: data.amounts,
+                        backgroundColor: 'rgba(53, 169, 21, 0.5)',
+                        borderColor: 'rgba(53, 169, 21, 1)',
+                        borderWidth: 1
                     }]
                 },
                 options: {
