438ab3126109542fd5e11365029f1d8495bc681f
akinl <akinlabisamson15@gmail.com>
2023-09-03 00:34:22 +0100

    chart

--- app/Console/encription.php
+++ app/Console/encription.php
@@ -6,9 +6,11 @@ class encription
 {
     public static function encryptdata($plain)
     {
-        $aeskey="A3BB18B5E19B1ACBE661AFFC228A01B4";
-        $ivkey="3AF472DB3BF7DCCB";
-        return bin2hex(openssl_encrypt($plain, "aes-128-cbc", $aeskey, OPENSSL_RAW_DATA,$ivkey));
+        $aeskey = "A3BB18B5E19B1ACBE661AFFC228A01B4";
+        $ivkey = "3AF472DB3BF7DCCB";
+
+        // Return the result as a string
+        return bin2hex(openssl_encrypt($plain, "aes-128-cbc", $aeskey, OPENSSL_RAW_DATA, $ivkey));
     }
 
     public static function decryptdata($encriptedData)
