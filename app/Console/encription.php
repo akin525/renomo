f5345b80b968c2c189da36b58dd14059426be194
akinl <akinlabisamson15@gmail.com>
2023-09-03 00:38:03 +0100

    chart

--- app/Console/encription.php
+++ app/Console/encription.php
@@ -6,19 +6,21 @@ class encription
 {
     public static function encryptdata($plain)
     {
-        $aeskey = "A3BB18B5E19B1ACBE661AFFC228A01B4";
-        $ivkey = "3AF472DB3BF7DCCB";
-
-        // Return the result as a string
-        return bin2hex(openssl_encrypt($plain, "aes-128-cbc", $aeskey, OPENSSL_RAW_DATA, $ivkey));
+        $aeskey="A3BB18B5E19B1ACBE661AFFC228A01B4";
+        $ivkey="3AF472DB3BF7DCCB";
+        return bin2hex(openssl_encrypt($plain, "aes-128-cbc", $aeskey, OPENSSL_RAW_DATA,$ivkey));
     }
 
     public static function decryptdata($encriptedData)
     {
-        $aeskey="A3BB18B5E19B1ACBE661AFFC228A01B4";
-        $ivkey="3AF472DB3BF7DCCB";
+        $aeskey = "A3BB18B5E19B1ACBE661AFFC228A01B4";
+        $ivkey = "3AF472DB3BF7DCCB";
         $ciphertext = hex2bin($encriptedData);
-        return openssl_decrypt($ciphertext, "aes-128-cbc",$aeskey, OPENSSL_RAW_DATA, $ivkey);
+
+        // Cast the result to a string using (string)
+        $decryptedData = (string) openssl_decrypt($ciphertext, "aes-128-cbc", $aeskey, OPENSSL_RAW_DATA, $ivkey);
+
+        return $decryptedData;
     }
 
 }
